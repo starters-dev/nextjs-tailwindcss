064f582ad9865290a00c655b90263c9b74ebf545
Batr Kanzitelli <kanzitdinov@gmail.com>
2022-02-22 16:56:30 +0100

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+import { Container, Content } from "../components/Container";
+
+const Page404 = () => {
+  useEffect(() => {
+    start();
+  }, []);
+
+  const start = async () => {
+    setLoading(true);
+
+    setLoading(false);
+  };
+
+  // State
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <Container title="404 Not Found">
+      <Content className="text-center">
+        <h1 className="text-xl font-semibold">Page is not found</h1>
+      </Content>
+    </Container>
+  );
+};
+
+export default Page404;
