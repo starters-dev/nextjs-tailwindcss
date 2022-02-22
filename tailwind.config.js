414af71ee969859383c340a6414402e651bac772
Batr Kanzitelli <kanzitdinov@gmail.com>
2022-02-22 16:39:11 +0100

    tailwind integration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffc6c7",
+      },
+      fontFamily: {
+        coolvetica: ["Coolvetica"],
+      },
+    },
+  },
+  plugins: [],
+};
