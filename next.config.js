224808601899c0a70587340a93238303bf6b5f19
Batr Kanzitelli <kanzitdinov@gmail.com>
2022-02-22 19:29:25 +0100

    Docker file added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  // needed for Next.js + Docker -- https://github.com/vercel/next.js/tree/canary/examples/with-docker#using-docker
+  experimental: {
+    outputStandalone: true,
+  },
+};
+
+module.exports = nextConfig;
